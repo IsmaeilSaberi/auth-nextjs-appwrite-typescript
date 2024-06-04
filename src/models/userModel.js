6e2468f1b39f3883f8a127a43ef267708b776992
IsmaeilSaberi <ismaeilsaberi@gmail.com>
2024-06-03 23:22:43 -0700

    I create user model

---
+++ src/models/userModel.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import { type } from "os";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Please provide a username"],
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide an email"],
+    unique: true,
+  },
+  password: { type: String, required: [true, "Please provide a password"] },
+  isVerified: { type: Boolean, default: false },
+  isAdmin: { type: Boolean, default: false },
+  forgotPasswordToken: String,
+  forgotPasswordTokenExpiry: Date,
+  verifyToken: String,
+  verifyTokenExpiry: Date,
+});
+
+const User = mongoose.models.users || mongoose.model("users", userSchema);
+
+export default User;
