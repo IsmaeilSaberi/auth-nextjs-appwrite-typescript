0fc10ce0d51b7fa039984cd4d13ce00c39a73c2f
IsmaeilSaberi <ismaeilsaberi@gmail.com>
2024-06-07 22:45:21 -0700

    complete the verifying of user with email

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -37,9 +37,8 @@ export const sendEmail = async ({ email, emailType, userId }: any) => {
         process.env.DOMAIN
       }/verifyemail?token=${hashedToken}">here</a> to ${
         emailType === "VERIFY" ? "Verify your email" : "Reset your password"
-      } or copy and paste the link below in your browser. <br> ${
-        process.env.DOMAIN
-      }/verifyemail?token=${hashedToken}
+      } or copy and paste the link below in your browser. <br>
+       ${process.env.DOMAIN}/verifyemail?token=${hashedToken}
       </p>`,
     };
 
